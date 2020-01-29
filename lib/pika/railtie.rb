ec3b9b5ce883d826be19f4c7d4d50b96064b4d44
Steve Jabour <steve@jabour.me>
2020-01-29 08:12:37 -0700

    Added logger to railtie config

--- lib/pika/railtie.rb
+++ lib/pika/railtie.rb
@@ -1,6 +1,7 @@
 module Pika
   class Railtie < ::Rails::Railtie
     initializer 'pika.initialize', after: 'dry.env.initialize' do
+      Pika.config.logger = Rails.logger
       Pika.config.tasks.root = Rails.root.join('app', 'tasks')
       Pika.config.instance = Runner.new
     end
